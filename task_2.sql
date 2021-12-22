d919c3fe25b5139c00756448d02df4f9435c3842
coskun duman <dumanc753@gmail.com>
2021-12-22 21:36:52 +0300

    task_2 file created

---
+++ task_2.sql
@@ -0,0 +1,12 @@
+/* ÖDEV: Chinook veritabanındaki tracks tablosunda bulunan her bir şarkının türü (genre)
+listeleyiniz.*/
+
+	 
+ /* ÖDEV: invoice tablosundaki faturaların her birinin müşteri adını (FirstName),
+ soyadını (lastName), fatura tarihi (InvoiceDate) ve fatura meblağını (total) 
+ listeleyen sorguyu yazınız */
+ 
+	
+	/* ÖDEV: artists tablosunda bulunan her bir kişinin albums tablosunda 
+bulunan tüm albümlerinin listeleyen sorguyu yazınız. 
+Sorguda ArtistId, Name, Title ve AlbumId olmalıdır */
