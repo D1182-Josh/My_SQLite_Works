df24f8fcaf391ed67cf2a1d4b55491bd9213208b
coskun duman <dumanc753@gmail.com>
2021-12-22 21:49:08 +0300

    first task code was written

--- task_2.sql
+++ task_2.sql
@@ -1,7 +1,13 @@
 /* ÖDEV: Chinook veritabanındaki tracks tablosunda bulunan her bir şarkının türü (genre)
 listeleyiniz.*/
-
-	 
+	SELECT * FROM tracks;
+	SELECT * FROM genres;
+	
+	SELECT t.name as Song_Name, g.name as Song_Genre
+	FROM tracks t
+	JOIN genres g
+	ON t.GenreId = g.GenreId;
+ 
  /* ÖDEV: invoice tablosundaki faturaların her birinin müşteri adını (FirstName),
  soyadını (lastName), fatura tarihi (InvoiceDate) ve fatura meblağını (total) 
  listeleyen sorguyu yazınız */
