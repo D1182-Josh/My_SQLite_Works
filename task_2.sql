c78e25eccd2e7b53b6a4a47591b8e9241d7f45fe
coskun duman <dumanc753@gmail.com>
2021-12-23 11:12:17 +0300

    task_2's  codes were update

--- task_2.sql
+++ task_2.sql
@@ -11,8 +11,23 @@ listeleyiniz.*/
  /* ÖDEV: invoice tablosundaki faturaların her birinin müşteri adını (FirstName),
  soyadını (lastName), fatura tarihi (InvoiceDate) ve fatura meblağını (total) 
  listeleyen sorguyu yazınız */
- 
+	SELECT * FROM invoices;
+	SELECT * FROM customers;
+	
+	SELECT c.FirstName, c.LastName, i.InvoiceDate, i.total
+	FROM invoices i
+	JOIN customers c
+	ON i.CustomerId=c.CustomerId;
 	
 	/* ÖDEV: artists tablosunda bulunan her bir kişinin albums tablosunda 
 bulunan tüm albümlerinin listeleyen sorguyu yazınız. 
 Sorguda ArtistId, Name, Title ve AlbumId olmalıdır */
+
+	
+	SELECT * FROM artists;
+	SELECT * FROM albums;
+	
+	SELECT ar.ArtistId, ar.name, al.Title, al.AlbumId
+	FROM artists ar
+	JOIN albums al
+	ON ar.ArtistId=al.ArtistId;
