382afd46843375909d9337ad98f3d92508fa4c61
coskun duman <dumanc753@gmail.com>
2021-12-24 15:25:27 +0300

    tasks were written

---
+++ Assignment_1.sql
@@ -0,0 +1,19 @@
+/*---Questions:
+1. Write a query that displays InvoiceId, CustomerId and total dollar amount for each invoice, sorted first by CustomerId (in ascending order), and then by total dollar amount  (in descending order).
+
+2. Write a query that displays InvoiceId, CustomerId and total dollar amount for each invoice, but this time sorted first by total dollar amount (in descending order), and then by CustomerId (in ascending order).
+
+3. Compare the results of these two queries above. How are the results different when you switch the column you sort on first? (Explain it in your own words.)
+
+4. Write a query to pull the first 10 rows and all columns from the invoices table that have a dollar amount of total greater than or equal to 10.
+
+5. Write a query to pull the first 5 rows and all columns from the invoices table that have a dollar amount of total less than 10.
+
+6. Find all track names that start with 'B' and end with 's'.
+
+7. Use the invoices table to find all information regarding invoices whose billing address is USA or Germany or Norway or Canada and invoice date is at any point in 2010, sorted from newest to oldest.
+
+SOLUTİON*/
+
+
+
