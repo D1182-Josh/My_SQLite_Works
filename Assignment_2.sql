76a69932db91818b2649deb6d6c69b0fdaaa0850
coskun duman <dumanc753@gmail.com>
2021-12-24 15:33:15 +0300

    tasks were written in it

---
+++ Assignment_2.sql
@@ -0,0 +1,12 @@
+/*-----Questions:----
+1- How many tracks does each album have? Your solution should include Album id and its number of tracks sorted from highest to lowest.
+
+2- Find the album title of the tracks. Your solution should include track name and its album title.
+
+3- Find the minimum duration of the track in each album. Your solution should include album id, album title and duration of the track sorted from highest to lowest.
+
+4- Find the total duration of each album. Your solution should include album id, album title and its total duration sorted from highest to lowest.
+
+5- Based on the previous question, find the albums whose total duration is higher than 70 minutes. Your solution should include album title and total duration.
+
+
