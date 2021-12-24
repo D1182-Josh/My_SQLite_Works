a530797a132d5cbb1de0793270249dc493d0d455
coskun duman <dumanc753@gmail.com>
2021-12-24 15:36:51 +0300

    solutions were written in the file

--- Assignment_2.sql
+++ Assignment_2.sql
@@ -9,4 +9,37 @@
 
 5- Based on the previous question, find the albums whose total duration is higher than 70 minutes. Your solution should include album title and total duration.
 
+/*--SOLUTİONS--*/
 
+
+-- 1
+SELECT AlbumId, count(name) as tracks_number
+FROM tracks
+GROUP BY AlbumId;
+
+-- 2
+SELECT tracks.name,albums.Title
+FROM tracks
+JOIN albums ON tracks.AlbumId = albums.AlbumId;
+
+-- 3
+
+SELECT tracks.AlbumId,albums.Title,min(Milliseconds) as minimum
+FROM albums
+JOIN tracks ON albums.ArtistId = tracks.AlbumId
+GROUP BY Title
+ORDER BY minimum DESC
+
+-- 4
+SELECT tracks.AlbumId,albums.Title,SUM(Milliseconds) as total
+FROM albums
+JOIN tracks ON albums.ArtistId = tracks.AlbumId
+GROUP BY Title
+ORDER BY total DESC
+
+-- 5
+SELECT albums.Title,SUM(Milliseconds) as total
+FROM albums
+JOIN tracks ON albums.ArtistId = tracks.AlbumId
+GROUP BY Title
+HAVING total > 4200000
